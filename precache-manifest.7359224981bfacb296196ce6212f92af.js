cabfda8a0b7c8f0c964049314c68de94a3bd49da
Irshad Ali <irshadali786143@gmail.com>
2019-11-07 23:52:06 +0530

    V 2.0.1

---
+++ precache-manifest.7359224981bfacb296196ce6212f92af.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d829aa04fb2982c234ee960148c3969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "951c76b9f529658c6741",
+    "url": "/static/css/main.4c86bf7d.chunk.css"
+  },
+  {
+    "revision": "ae1a2a8aa0455074ed6f",
+    "url": "/static/js/2.d7373fbe.chunk.js"
+  },
+  {
+    "revision": "951c76b9f529658c6741",
+    "url": "/static/js/main.e507fb2c.chunk.js"
+  },
+  {
+    "revision": "a607ab31fb6f5e2a547a",
+    "url": "/static/js/runtime-main.d63242a5.js"
+  },
+  {
+    "revision": "8a7e565b495af00093acc8109b5ad51e",
+    "url": "/static/media/Courier-10-Pitch-Bold.8a7e565b.ttf"
+  },
+  {
+    "revision": "76f1c070153b1063a0013689aa86b2aa",
+    "url": "/static/media/courier-prime-sans.76f1c070.ttf"
+  },
+  {
+    "revision": "31e8e444d9a5a1854bb672cea8f5a524",
+    "url": "/static/media/parsegroteskmono_bold.31e8e444.otf"
+  },
+  {
+    "revision": "77d97cb45842e6bbe460ca7410ad9a9e",
+    "url": "/static/media/parsegroteskmono_regular.77d97cb4.otf"
+  },
+  {
+    "revision": "52caea8c181fb7afa2182f33220119e6",
+    "url": "/static/media/parsegroteskmono_thin.52caea8c.otf"
+  }
+]);
