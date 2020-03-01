621aaf4120a56bda3aa8c023b36a4e27fa70cf51
irshad ali <irshadali786143@gmail.com>
2020-03-01 13:17:31 +0530

    Uses Added

---
+++ precache-manifest.90ef3399aa84140ca4615a4ee8afb6d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebae9a6e64fa1ddb19b7c6d89107e0fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efb44868eeb8e566237b",
+    "url": "/static/css/main.08d3a049.chunk.css"
+  },
+  {
+    "revision": "a696fea76a48f5b6e3e5",
+    "url": "/static/js/2.2efe7563.chunk.js"
+  },
+  {
+    "revision": "efb44868eeb8e566237b",
+    "url": "/static/js/main.debd8686.chunk.js"
+  },
+  {
+    "revision": "a607ab31fb6f5e2a547a",
+    "url": "/static/js/runtime-main.d63242a5.js"
+  },
+  {
+    "revision": "8a7e565b495af00093acc8109b5ad51e",
+    "url": "/static/media/Courier-10-Pitch-Bold.8a7e565b.ttf"
+  },
+  {
+    "revision": "76f1c070153b1063a0013689aa86b2aa",
+    "url": "/static/media/courier-prime-sans.76f1c070.ttf"
+  },
+  {
+    "revision": "31e8e444d9a5a1854bb672cea8f5a524",
+    "url": "/static/media/parsegroteskmono_bold.31e8e444.otf"
+  },
+  {
+    "revision": "77d97cb45842e6bbe460ca7410ad9a9e",
+    "url": "/static/media/parsegroteskmono_regular.77d97cb4.otf"
+  },
+  {
+    "revision": "52caea8c181fb7afa2182f33220119e6",
+    "url": "/static/media/parsegroteskmono_thin.52caea8c.otf"
+  }
+]);
